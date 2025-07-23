0409f459e8a9e5b2699ae6eed6706746b72a2456
TRISHAN00 <trishansaha43@gmail.com>
2025-07-24 03:24:17 +0600

    practice typeOf instance and array

--- SRC/learn-type.ts
+++ SRC/learn-type.ts
@@ -5,3 +5,42 @@ let address: string = `${myName} stays in BD`;
 let amIMad: boolean = false;
 
 let x: any = true;
+
+let multipleTypes: string | number = "Trisha";
+
+multipleTypes = 30;
+
+if (typeof myName === "string") {
+  console.log("Yes this is a string");
+} else {
+  console.log("No this is not string");
+}
+
+class Car {
+  name: string;
+  constructor(input: string) {
+    this.name = input;
+  }
+}
+
+class Human {
+  name: string;
+  constructor(input: string) {
+    this.name = input;
+  }
+}
+
+const bmw = new Car("BMW Car");
+
+if (bmw instanceof Human) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
+let str: any = "I am string created with any";
+let index: number = (str as string).indexOf("a");
+let index1: number = (<String>str).indexOf("a");
+
+let names: string[] = ["Trishan", "Rishan"];
+let ages: Array<number> = [12, 14, 16];
